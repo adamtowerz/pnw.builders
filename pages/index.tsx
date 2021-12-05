07804730f04039b726b1f1ac898ff510d206b23b
Adam Towers <adam@Adams-MacBook-Pro.local>
2021-12-04 16:33:45 -0800

    base page teaser

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>pnw.builders</title>
+        <meta name="description" content="maybe something" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          ⛰️ pnw.builders ⛰️
+        </h1>
+      </main>
+
+      {/* <footer className={styles.footer}>
+        maybe something
+      </footer> */}
+    </div>
+  )
+}
+
+export default Home
